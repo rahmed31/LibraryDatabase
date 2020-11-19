d7be7d7a11a536af32745ea0a529ae63d867fe9a
Raihan Ahmed <raihan.ahmed117@gmail.com>
2020-11-19 17:35:33 -0600

    add db info file

---
+++ src/dbInfo.java
@@ -0,0 +1,11 @@
+public class dbInfo {
+    private static String username = "raihanahmed";
+    private static String password = "Cuzuqybc0917!";
+
+    public static String getUsername() {
+        return username;
+    }
+    public static String getPassword() {
+        return password;
+    }
+}
