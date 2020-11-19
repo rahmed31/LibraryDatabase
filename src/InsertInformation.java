2d9304b0caf88dccfbe681e17e1a391defd727e4
Raihan Ahmed <raihan.ahmed117@gmail.com>
2020-11-19 04:59:36 -0600

    added ability to insert random cardholder data

--- src/InsertInformation.java
+++ src/InsertInformation.java
@@ -3,6 +3,8 @@ import com.opencsv.CSVReader;
 import java.io.FileReader;
 import java.sql.*;
 import java.io.File;
+import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 import java.util.Random;
@@ -11,7 +13,7 @@ import java.io.FileNotFoundException;
 public class InsertInformation {
     public static void main(String[] args) throws Exception {
         try {
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/LibraryDatabase?serverTimezone=UTC", "user","password");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/LibraryDatabase?serverTimezone=UTC", "raihanahmed","Cuzuqybc0917!");
             System.out.println("Connection successful!");
 
             /* Record creation*/
@@ -90,6 +92,109 @@ public class InsertInformation {
                 e.printStackTrace();
             }
 
+            //Insert cardHolder data
+
+            String[] firstNames = new String[50];
+            String[] lastNames = new String[50];
+            String[] addresses = new String[50];
+            double[] phoneNumbers = new double[50];
+
+            ArrayList<String> first = new ArrayList<String>();
+            ArrayList<String> last = new ArrayList<String>();
+
+            try {
+                File myObj = new File("/Users/raihanahmed/IdeaProjects/LibraryDatabase/lib/first-names.txt");
+                File myObj2 = new File("/Users/raihanahmed/IdeaProjects/LibraryDatabase/lib/last-names.txt");
+                File myObj3 = new File("/Users/raihanahmed/IdeaProjects/LibraryDatabase/lib/random-addresses.txt");
+                File myObj4 = new File("/Users/raihanahmed/IdeaProjects/LibraryDatabase/lib/random-phonenumbers.txt");
+                Scanner reader = new Scanner(myObj);
+                Scanner reader2 = new Scanner(myObj2);
+                Scanner reader3 = new Scanner(myObj3);
+                Scanner reader4 = new Scanner(myObj4);
+
+                while (reader.hasNextLine()) {
+                    String data = reader.nextLine();
+                    first.add(data);
+                }
+                reader.close();
+
+                while (reader2.hasNextLine()) {
+                    String data = reader2.nextLine();
+                    String output = data.substring(0, 1) + data.substring(1).toLowerCase();
+                    last.add(output);
+                }
+                reader2.close();
+
+                int i = 0;
+                while (reader3.hasNextLine()) {
+                    String data = reader3.nextLine();
+                    addresses[i] = data;
+                    i++;
+                }
+                reader3.close();
+
+                int j = 0;
+                while (reader4.hasNext()) {
+                    double data = reader4.nextDouble();
+                    phoneNumbers[j] = data;
+                    j++;
+                }
+                reader3.close();
+
+            }
+            catch (Exception e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+
+            int max1 = first.size();
+            int max2 = last.size();
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < 50; i++) {
+                int random = rnd.nextInt(max1 - 1 - 1 + 1);
+                int random2 = rnd.nextInt(max2 - 1 - 1 + 1);
+                String firstName = first.get(random);
+                String lastName = last.get(random2);
+
+                firstNames[i] = firstName;
+                lastNames[i] = lastName;
+            }
+
+            try {
+                String query = "INSERT INTO Cardholder VALUES (?, ?, ?, ?, ?, ?)";
+                PreparedStatement preparedStmt = con.prepareStatement(query);
+
+                int count = 0;
+                Random rnd2 = new Random();
+
+                while(count < 50) {
+                    int random = rnd.nextInt(49 - 0 + 1);
+                    int cardNumber = rnd.nextInt( 50000 - 10000 + 1) + 10000;
+                    String lastName = lastNames[random];
+                    String firstName = firstNames[random];
+                    String address = addresses[count];
+                    double phoneNumber = phoneNumbers[count];
+                    int type = rnd.nextInt(3 - 1 + 1) + 1;
+                    String email = generateEmail(firstName, lastName, type);
+
+                    preparedStmt.setInt(1, cardNumber);
+                    preparedStmt.setString(2, lastName);
+                    preparedStmt.setString(3, firstName);
+                    preparedStmt.setString(4, address);
+                    preparedStmt.setDouble(5, phoneNumber);
+                    preparedStmt.setString(6, email);
+                    preparedStmt.execute();
+                    count++;
+                }
+
+            }
+            catch (Exception e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+
             con.close();
 
             if (con.isClosed()) {
@@ -102,5 +207,23 @@ public class InsertInformation {
         finally {
             System.out.println("--------------Insertions finished--------------");
         }
+    }
+
+    public static String generateEmail(String firstName, String lastName, int type) {
+        String email = "";
+        Random rnd = new Random();
+        String[] domains = {"@gmail.com", "@outlook.com", "@yahoo.com", "@icloud.com", "@aol.com", "@comcast.net", "@live.com"};
+
+        if (type == 1) {
+            email = firstName.toLowerCase() + "." + lastName.toLowerCase() + domains[rnd.nextInt(6-1+1) + 1];
+        }
+        else if (type == 2) {
+            email = firstName.substring(0, 1).toLowerCase() + lastName.toLowerCase() + String.valueOf(rnd.nextInt(20-1+1) + 1) + domains[rnd.nextInt(6-1+1) + 1];
+        }
+        else if (type == 3) {
+            email = lastName.toLowerCase() + String.valueOf(rnd.nextInt(20-1+1) + 1) + domains[rnd.nextInt(6-1+1) + 1];
+        }
+
+        return email;
     }
 }
