afe5c90f1e82700962009914797192b526b0ed79
Raihan Ahmed <raihan.ahmed117@gmail.com>
2020-11-21 00:57:50 -0600

    updated script to create database

--- queries/DBProjectScript.sql
+++ queries/DBProjectScript.sql
@@ -6,7 +6,7 @@ USE LibraryDatabase;
 
 -- create new tables with optional constraints --
 CREATE TABLE Cardholder (
-cardNumber INTEGER NOT NULL AUTO_INCREMENT,
+cardNumber INTEGER NOT NULL,
 lastName VARCHAR(40) NOT NULL,
 firstName VARCHAR(40) NOT NULL,
 address VARCHAR(100),
@@ -16,36 +16,36 @@ PRIMARY KEY (cardNumber)
 );
 
 CREATE TABLE Publisher (
-publisherID INTEGER NOT NULL AUTO_INCREMENT,
+publisherID INTEGER NOT NULL,
 publisherName VARCHAR(40) NOT NULL,
 PRIMARY KEY(publisherID)
 );
 
-CREATE TABLE Employee (
-employeeID INTEGER NOT NULL AUTO_INCREMENT,
-lastName VARCHAR(40) NOT NULL,
-firstName VARCHAR(40) NOT NULL,
-deskNumber INTEGER NOT NULL,
-salary INTEGER NOT NULL,
-PRIMARY KEY (employeeID)
+CREATE TABLE Location (
+floorNumber INTEGER NOT NULL,
+studyrooms INTEGER NOT NULL,
+computers INTEGER NOT NULL,
+availableRooms INTEGER NOT NULL,
+availableComputers INTEGER NOT NULL,
+PRIMARY KEY(floorNumber)
 );
 
-CREATE TABLE Location (
-bookIndex VARCHAR(20) NOT NULL,
-supervisorID INTEGER NOT NULL,
-floor INTEGER NOT NULL,
-section VARCHAR(40),
-aisle VARCHAR(10),
-shelf INTEGER,
-PRIMARY KEY(bookIndex),
-FOREIGN KEY (supervisorID) REFERENCES Employee(employeeID) ON UPDATE CASCADE
+CREATE TABLE Employee (
+employeeID INTEGER NOT NULL,
+lastName VARCHAR(40) NOT NULL,
+firstName VARCHAR(40) NOT NULL,
+extension INTEGER,
+floorNumber INTEGER NOT NULL,
+salary INTEGER NOT NULL,
+PRIMARY KEY (employeeID),
+FOREIGN KEY (floorNumber) REFERENCES Location(floorNumber)
 );
 
 -- figure out way to update available copies using generated columns maybe? --
 CREATE TABLE Book (
 bookID INTEGER NOT NULL AUTO_INCREMENT,
-publisherID INTEGER,
-bookIndex VARCHAR(20),  
+publisherID INTEGER NOT NULL,
+floorLocation INTEGER NOT NULL,  
 ISBN VARCHAR(20) NOT NULL,
 title VARCHAR(250) NOT NULL,
 author VARCHAR(50) NOT NULL,
@@ -55,20 +55,27 @@ publicationYear INTEGER NOT NULL,
 bookInfo MEDIUMTEXT NOT NULL,
 CHECK (copies > 0),
 PRIMARY KEY (bookID),
-FOREIGN KEY (publisherID) REFERENCES Publisher(publisherID),
-FOREIGN KEY (bookIndex) REFERENCES Location(bookIndex) ON DELETE CASCADE ON UPDATE CASCADE
+FOREIGN KEY (floorLocation) REFERENCES Location(floorNumber),
+FOREIGN KEY (publisherID) REFERENCES Publisher(publisherID)
 );
 
 CREATE TABLE Borrows (
 borrowerID INTEGER NOT NULL,
 bookID INTEGER NOT NULL,
--- bookTitle VARCHAR(100) NOT NULL, can delete this soon
 borrowDate DATE NOT NULL,
 dueDate DATE NOT NULL,
 returnDate DATE NULL,
 CHECK (dueDate > borrowDate),
-FOREIGN KEY (borrowerID) REFERENCES Cardholder(cardNumber) ON DELETE CASCADE,
-FOREIGN KEY (bookID) REFERENCES Book(bookID)
+FOREIGN KEY (borrowerID) REFERENCES Cardholder(cardNumber) ON UPDATE CASCADE,
+FOREIGN KEY (bookID) REFERENCES Book(bookID) ON UPDATE CASCADE
 );
 
+CREATE TABLE Reserves (
+reservationID INTEGER NOT NULL,
+floorLocation INTEGER NOT NULL,
+computer BOOLEAN,
+studyroom BOOLEAN,
+PRIMARY KEY (reservationID),
+FOREIGN KEY (floorLocation) REFERENCES Location(floorNumber)
+);
 
