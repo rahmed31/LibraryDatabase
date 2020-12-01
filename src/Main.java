9ee309e00dea691e729e6bacb32e9551f6088785
Raihan Ahmed <raihan.ahmed117@gmail.com>
2020-11-30 21:49:12 -0600

    added all 8 sql queries

--- src/Main.java
+++ src/Main.java
@@ -28,9 +28,9 @@ public class Main {
             System.out.println("1. Select the publishers, and count of books that each publisher has in the database, that has at least 20 books in the library's inventory.");
             System.out.println("2. Find the count of employees working on each floor grouped by floor number.");
             System.out.println("3. Find the last name, first name, and email address of each library member that has checked out a book and not returned it.");
-            System.out.println("4. Find the last name, first name, and email address of each library member that has a book checked out within two days of its return date.");
+            System.out.println("4. Find the last name, first name, and email address of each library member that has a book checked out and is within two days of its return date.");
             System.out.println("5. Find the last name, first name, and email address of each library member that returned a book late and the amount of days the book was late.");
-            System.out.println("6. Find the amount of available study rooms on the 3rd floor.");
+            System.out.println("6. Find the amount of available study rooms on the 1st floor.");
             System.out.println("7. Find the names of the employees, and their salaries, that make more than the average employee salary.");
             System.out.println("8. Find the extensions of the employees that can help with finding a book on the 2nd floor.");
 
@@ -63,81 +63,87 @@ public class Main {
                     System.out.println();
                 }
                 else if (query == 2) {
-                    results = stmt.executeQuery("SELECT publisherName, COUNT(bookID) FROM Publisher JOIN Book ON Book.publisherID = Publisher.publisherID\n" +
-                            "GROUP BY publisherName\n" +
-                            "HAVING COUNT(bookID) >= 20;");
+                    results = stmt.executeQuery("SELECT lastName, firstName, email\n" +
+                            "FROM Cardholder\n" +
+                            "JOIN Borrows ON Cardholder.cardNumber = Borrows.borrowerID\n" +
+                            "WHERE returnDate IS NULL GROUP BY cardNumber\n" +
+                            "ORDER BY lastName;");
 
-                    System.out.printf("%-20.20s  %-20.20s%n", "Publisher Name", "Book Count");
+                    System.out.printf("%-20.20s  %-20.20s %-20.30s%n", "Last Name", "First Name", "Email Address");
                     while(results.next()) {
-                        System.out.printf("%-20.20s  %-20.20s%n", results.getString("publisherName"), results.getInt("COUNT(bookID)"));
+                        System.out.printf("%-20.20s %-20.20s %-20.30s%n", results.getString("lastName"), results.getString("firstName"), results.getString("email"));
                     }
                     System.out.println();
                 }
                 else if (query == 3) {
-//                    results = stmt.executeQuery("SELECT publisherName, COUNT(bookID) FROM Publisher JOIN Book ON Book.publisherID = Publisher.publisherID\n" +
-//                            "GROUP BY publisherName\n" +
-//                            "HAVING COUNT(bookID) >= 20;");
-//
-//                    System.out.printf("%-20.20s  %-20.20s%n", "Publisher Name", "Book Count");
-//                    while(results.next()) {
-//                        System.out.printf("%-20.20s  %-20.20s%n", results.getString("publisherName"), results.getInt("COUNT(bookID)"));
-//                    }
-//                    System.out.println();
+                    results = stmt.executeQuery("SELECT lastName, firstName, email\n" +
+                            "FROM Cardholder\n" +
+                            "JOIN Borrows ON Cardholder.cardNumber = Borrows.borrowerID\n" +
+                            "WHERE MONTH(returnDate) - MONTH(dueDate) = 0 AND DAY(returnDate) - DAY(dueDate) > 0 GROUP BY cardNumber\n" +
+                            "ORDER BY lastName;");
+
+                    System.out.printf("%-20.20s  %-20.20s %-20.30s%n", "Last Name", "First Name", "Email Address");
+                    while(results.next()) {
+                        System.out.printf("%-20.20s %-20.20s %-20.30s%n", results.getString("lastName"), results.getString("firstName"), results.getString("email"));
+                    }
+                    System.out.println();
                 }
                 else if (query == 4) {
-//                    results = stmt.executeQuery("SELECT publisherName, COUNT(bookID) FROM Publisher JOIN Book ON Book.publisherID = Publisher.publisherID\n" +
-//                            "GROUP BY publisherName\n" +
-//                            "HAVING COUNT(bookID) >= 20;");
-//
-//                    System.out.printf("%-20.20s  %-20.20s%n", "Publisher Name", "Book Count");
-//                    while(results.next()) {
-//                        System.out.printf("%-20.20s  %-20.20s%n", results.getString("publisherName"), results.getInt("COUNT(bookID)"));
-//                    }
-//                    System.out.println();
+                    results = stmt.executeQuery("SELECT floorNumber, COUNT(employeeID)\n" +
+                            "FROM Employee\n" +
+                            "GROUP BY floorNumber;");
+
+                    System.out.printf("%-20.20s  %-20.20s%n", "Floor", "Number of Employees");
+                    while(results.next()) {
+                        System.out.printf("%-20.20s  %-20.20s%n", results.getInt("floorNumber"), results.getInt("COUNT(employeeID)"));
+                    }
+                    System.out.println();
                 }
                 else if (query == 5) {
-//                    results = stmt.executeQuery("SELECT publisherName, COUNT(bookID) FROM Publisher JOIN Book ON Book.publisherID = Publisher.publisherID\n" +
-//                            "GROUP BY publisherName\n" +
-//                            "HAVING COUNT(bookID) >= 20;");
-//
-//                    System.out.printf("%-20.20s  %-20.20s%n", "Publisher Name", "Book Count");
-//                    while(results.next()) {
-//                        System.out.printf("%-20.20s  %-20.20s%n", results.getString("publisherName"), results.getInt("COUNT(bookID)"));
-//                    }
-//                    System.out.println();
+                    results = stmt.executeQuery("SELECT (SELECT studyrooms FROM Location WHERE floorNumber = 1) - (SELECT COUNT(studyroom) FROM Reserves WHERE floorLocation = 1 AND studyroom = 1) AS Difference;");
+
+                    System.out.print("Available studyrooms on the first floor: ");
+                    while(results.next()) {
+                        System.out.println(results.getInt(1));
+                    }
+                    System.out.println();
                 }
                 else if (query == 6) {
-//                    results = stmt.executeQuery("SELECT publisherName, COUNT(bookID) FROM Publisher JOIN Book ON Book.publisherID = Publisher.publisherID\n" +
-//                            "GROUP BY publisherName\n" +
-//                            "HAVING COUNT(bookID) >= 20;");
-//
-//                    System.out.printf("%-20.20s  %-20.20s%n", "Publisher Name", "Book Count");
-//                    while(results.next()) {
-//                        System.out.printf("%-20.20s  %-20.20s%n", results.getString("publisherName"), results.getInt("COUNT(bookID)"));
-//                    }
-//                    System.out.println();
+                    results = stmt.executeQuery("SELECT lastName, firstName FROM Employee WHERE salary > (SELECT AVG(salary) FROM Employee);");
+
+                    System.out.printf("%-20.20s  %-20.20s%n", "Last Name", "First Name");
+                    while(results.next()) {
+                        System.out.printf("%-20.20s  %-20.20s%n", results.getString("lastName"), results.getString("firstName"));
+                    }
+                    System.out.println();
                 }
                 else if (query == 7) {
-//                    results = stmt.executeQuery("SELECT publisherName, COUNT(bookID) FROM Publisher JOIN Book ON Book.publisherID = Publisher.publisherID\n" +
-//                            "GROUP BY publisherName\n" +
-//                            "HAVING COUNT(bookID) >= 20;");
-//
-//                    System.out.printf("%-20.20s  %-20.20s%n", "Publisher Name", "Book Count");
-//                    while(results.next()) {
-//                        System.out.printf("%-20.20s  %-20.20s%n", results.getString("publisherName"), results.getInt("COUNT(bookID)"));
-//                    }
-//                    System.out.println();
+                    results = stmt.executeQuery("SELECT lastName, firstName, extension FROM Employee WHERE floorNumber = (SELECT floorLocation FROM Book WHERE title = 'Goodbye to the Buttermilk Sky');");
+
+                    System.out.printf("%-20.20s  %-20.20s %-20.20s%n", "Last Name", "First Name", "Extension");
+                    while(results.next()) {
+                        System.out.printf("%-20.20s  %-20.20s %-20.20s%n", results.getString("lastName"), results.getString("firstName"), results.getInt("extension"));
+                    }
+                    System.out.println();
                 }
                 else if (query == 8) {
-//                    results = stmt.executeQuery("SELECT publisherName, COUNT(bookID) FROM Publisher JOIN Book ON Book.publisherID = Publisher.publisherID\n" +
-//                            "GROUP BY publisherName\n" +
-//                            "HAVING COUNT(bookID) >= 20;");
-//
-//                    System.out.printf("%-20.20s  %-20.20s%n", "Publisher Name", "Book Count");
-//                    while(results.next()) {
-//                        System.out.printf("%-20.20s  %-20.20s%n", results.getString("publisherName"), results.getInt("COUNT(bookID)"));
-//                    }
-//                    System.out.println();
+                    results = stmt.executeQuery("SELECT Book.title, bookInfo\n" +
+                            "FROM Book\n" +
+                            "INNER JOIN \n" +
+                            "(SELECT title\n" +
+                            "FROM Borrows\n" +
+                            "LEFT JOIN Book ON Book.bookID = Borrows.bookID\n" +
+                            "GROUP BY title\n" +
+                            "ORDER BY COUNT(title) DESC\n" +
+                            "LIMIT 8) AS result\n" +
+                            "ON result.title = Book.title\n" +
+                            "ORDER BY Book.title ASC;");
+
+                    System.out.printf("%-75.75s  %-1000.1000s%n", "Book Title", "Book Info");
+                    while(results.next()) {
+                        System.out.printf("%-75.75s  %-1000.1000s%n", results.getString("Book.title"), results.getString("bookInfo"));
+                    }
+                    System.out.println();
                 }
             }
         }
