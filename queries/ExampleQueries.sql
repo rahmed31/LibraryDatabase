fa0c3f3a0a7e3f407e5a8b1795d6522e85bf3830
Raihan Ahmed <raihan.ahmed117@gmail.com>
2020-11-30 22:41:22 -0600

    reformated queries and added descriptions

--- queries/ExampleQueries.sql
+++ queries/ExampleQueries.sql
@@ -1,35 +1,56 @@
 USE LibraryDatabase;
 
--- find people, and their email addresses, who have checked out books that have not yet returned them -- 
+/* 
+NOTE: 
+The example statements above each query have been generalized to avoid redundancy. 
+However, the constructed queries are tailored such that the desirable output is yielded.
+*/
+
+-- Query 1 --
+-- Select the publishers, and count of books that each publisher has in the database, that have at least 20 books in the library's inventory --
+SELECT publisherName, COUNT(bookID) 
+FROM Publisher 
+JOIN Book ON Book.publisherID = Publisher.publisherID
+GROUP BY publisherName
+HAVING COUNT(bookID) >= 20
+ORDER BY COUNT(bookID) ASC;
+          
+-- Query 2 --
+-- Find the names and emails of people who have checked out books and not yet returned them -- 
 SELECT lastName, firstName, email, COUNT(borrowerID)
 FROM Cardholder
 JOIN Borrows ON Cardholder.cardNumber = Borrows.borrowerID
 WHERE returnDate IS NULL GROUP BY cardNumber
 ORDER BY lastName;
 
--- find people who returned a book late
+-- Query 3 --
+-- Find the names and emails of people who returned a book late --
 SELECT lastName, firstName, email, COUNT(borrowerID)
 FROM Cardholder
 JOIN Borrows ON Cardholder.cardNumber = Borrows.borrowerID
 WHERE MONTH(returnDate) - MONTH(dueDate) = 0 AND DAY(returnDate) - DAY(dueDate) > 0 GROUP BY cardNumber
 ORDER BY lastName;
 
--- find the count of employees working on each floor -- 
+-- Query 4 --
+-- Find the amount of employees working on each floor -- 
 SELECT floorNumber, COUNT(employeeID)
 FROM Employee
 GROUP BY floorNumber;
 
--- find the number of available studyrooms on the 3rd floor --
+-- Query 5 --
+-- Find the number of available studyrooms on the first floor --
 SELECT (SELECT studyrooms FROM Location WHERE floorNumber = 1) - (SELECT COUNT(studyroom) FROM Reserves WHERE floorLocation = 1 AND studyroom = 1) AS Difference;
 
--- find employees who make more than average salary --
-SELECT firstName, lastName FROM Employee WHERE salary > (SELECT AVG(salary) FROM Employee);
+-- Query 6 --
+-- Find the names of employees, and their salaries, that make more than the average employee salary. --
+SELECT firstName, lastName, salary FROM Employee WHERE salary > (SELECT AVG(salary) FROM Employee);
 
--- find the employees who can assist with finding "Goodbye to the Buttermilk Sky" -- 
+-- Query 7 --
+-- Find the names and extensions of employees that can help with finding the book 'Goodbye to the Buttermilk Sky' -- 
 SELECT firstName, lastName, extension FROM Employee WHERE floorNumber = (SELECT floorLocation FROM Book WHERE title = 'Goodbye to the Buttermilk Sky');
 
--- find the names of the top 8 checked out books and their information -- 
-
+-- Query 8 --
+-- Find the names of the top 8 most frequently checked out books of the month and their information -- 
 SELECT Book.title, bookInfo
 FROM Book
 INNER JOIN 
