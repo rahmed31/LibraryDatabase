1c0d4a6f265b24e2dd140c7d012ff1bb746dbff4
Raihan Ahmed <raihan.ahmed117@gmail.com>
2020-12-01 01:21:33 -0600

    removed unnecessary method

--- src/InsertBorrows.java
+++ src/InsertBorrows.java
@@ -123,8 +123,5 @@ public class InsertBorrows {
             System.out.println("--------------Insertions complete--------------");
         }
     }
-    public static int randBetween(int start, int end) {
-        return start + (int)Math.round(Math.random() * (end - start));
-    }
 }
 
